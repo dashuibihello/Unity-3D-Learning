ea3259b17dcd9fcb082ef8630e2cc32c929ebf7b
Kiddion <895118352@qq.com>
2018-04-03 17:03:02 +0800

    Update homework2

---
+++ homework2/Priests and Devils.md
@@ -0,0 +1,23 @@
+# Priests and Devils
+----
+
+## 1.规则
+Priests and Devils is a puzzle game in which you will help the Priests and Devils to cross the river within the time limit. There are 3 priests and 3 devils at one side of the river. They all want to get to the other side of this river, but there is only one boat and this boat can only carry two persons each time. And there must be one person steering the boat from one side to the other side. In the flash game, you can click on them to move them and click the go button to move the boat to the other direction. If the priests are out numbered by the devils on either side of the river, they get killed and the game is over. You can try it in many ways. Keep all priests alive! Good luck!
+
+翻译：牧师和魔鬼是一个益智游戏，在这个游戏中，你将帮助牧师和魔鬼在时间限制内过河。河的一边有三个牧师和三个魔鬼。他们都想去这条河的另一边，但是只有一条船，而且这条船每次只能带两个人。一定有一个人把船从一边转向另一边。在flash游戏中，你可以点击他们来移动他们，点击go按钮，将船移到另一个方向。如果祭司在河的两边都被魔鬼所控制，他们就会被杀死，游戏就结束了。你可以用很多的方法试试。把所有牧师活着!好运！
+
+## 2.游戏具体操作方式
+| 按钮        | 条件           | 动作  |
+| ------------- |:-------------:| -----:|
+|Devil gets on         |在船靠岸的边有恶魔，船有空位 |恶魔上船（优先坐在左边)|
+|Left gets off         |左侧位置有对象              |对象下船             |
+|Go to the other side  |船上有对象                 |前往对岸             | 
+|Right gets off        |右侧位置有对象              |对象下船            |
+|Priest gets on        |在船靠岸的边有牧师，船有空位 |恶魔上船（优先坐在左边)|
+## 3列出游戏中提及的事物（Objects）
+
+三个魔鬼（紫底红头）、三个牧师（黑底白头）、一艘船、背景（两岸、河）
+
+
+
+
---
+++ homework2/game_2/Assets/Scripts/FirstController.cs
@@ -0,0 +1,331 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class FirstController : MonoBehaviour, IUserAction, ISceneController
+{
+    //游戏从左开始
+    Stack<GameObject> priests_left = new Stack<GameObject>();
+    Stack<GameObject> priests_right = new Stack<GameObject>();
+    Stack<GameObject> devils_left = new Stack<GameObject>();
+    Stack<GameObject> devils_right = new Stack<GameObject>();
+    GameObject Boat;
+    GameObject[] boatchair = new GameObject[2];
+
+    Vector3 priestLeftPos = new Vector3(-16, 2, 0);
+    Vector3 priestRightPos = new Vector3(14, 2, 0);
+    Vector3 devilStartPos = new Vector3(-11, 2, 0);
+    Vector3 devilRightPos = new Vector3(9, 2, 0);
+
+    Vector3 BoatLeftPos = new Vector3(-4, 0, 0);
+    Vector3 BoatRightPos = new Vector3(4, 0, 0);
+
+    Vector3 BoatFirstClass = new Vector3(-0.25f, 1, 0);
+    Vector3 BoatSecondClass = new Vector3(0.25f, 1, 0);
+
+    Vector3 object_move = new Vector3(1, 0, 0);
+
+    public State state = State.LEFT;
+    public enum State { LEFT, LEFTTORIGHT, RIGHTTOLEFT, RIGHT, WIN, LOSE };
+    public enum Identity { PRIEST, DEVIL, NONE};
+    public Identity[] BoatIdentity = { Identity.NONE, Identity.NONE };
+
+    void Awake()
+    {
+        SSDirector director = SSDirector.getInstance();
+        director.currentSceneController = this;
+        director.currentSceneController.LoadResources();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setPostion(priests_left, priestLeftPos, object_move);
+        setPostion(devils_left, devilStartPos, object_move);
+        setPostion(priests_right, priestRightPos, object_move);
+        setPostion(devils_right, devilRightPos, object_move);
+
+        if (this.state == State.LEFTTORIGHT)
+        {
+            Boat.transform.position = Vector3.MoveTowards(Boat.transform.position, BoatRightPos, 3 * Time.deltaTime);
+            if (Boat.transform.position == BoatRightPos)
+            {
+                this.state = State.RIGHT;
+            }
+        }
+        else if (this.state == State.RIGHTTOLEFT)
+        {
+            Boat.transform.position = Vector3.MoveTowards(Boat.transform.position, BoatLeftPos, 3 * Time.deltaTime);
+            if (Boat.transform.position == BoatLeftPos)
+            {
+                this.state = State.LEFT;
+            }
+        }
+        else
+        {
+            judge();
+        }
+    }
+
+    void setPostion(Stack<GameObject> obj_stack, Vector3 first, Vector3 move)
+    {
+        GameObject[] temp = obj_stack.ToArray();
+        for (int i = 0; i < temp.Length; i++)
+        {
+            temp[i].transform.localPosition = first + i * move;
+        }
+    }
+
+    int getBoatPassenger()
+    {
+        int num = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            if (boatchair[i] == null)
+                num++;
+        }
+        return num;
+    }
+
+    public void RowingBoat()
+    {
+        if (getBoatPassenger() != 2)
+        {
+            if (this.state == State.LEFT)
+                this.state = State.LEFTTORIGHT;
+            else if (this.state == State.RIGHT)
+                this.state = State.RIGHTTOLEFT;
+        }
+    }
+
+    public void DevilGeton()
+    {
+        if (this.state == State.LEFT || this.state == State.RIGHT)
+            if (getBoatPassenger() != 0)
+            {
+                if (this.state == State.LEFT)
+                {
+                    if(devils_left.Count != 0)
+                    {
+                        GameObject temp = devils_left.Pop();
+                        temp.transform.parent = Boat.transform;
+                        if (boatchair[0] == null)
+                        {
+                            boatchair[0] = temp;
+                            temp.transform.localPosition = BoatFirstClass;
+                            BoatIdentity[0] = Identity.DEVIL;
+                        }
+                        else
+                        {
+                            boatchair[1] = temp;
+                            temp.transform.localPosition = BoatSecondClass;
+                            BoatIdentity[1] = Identity.DEVIL;
+                        }
+                    }
+                }
+                else
+                {
+                    if(devils_right.Count != 0)
+                    {
+                        GameObject temp = devils_right.Pop();
+                        temp.transform.parent = Boat.transform;
+                        if (boatchair[0] == null)
+                        {
+                            boatchair[0] = temp;
+                            temp.transform.localPosition = BoatFirstClass;
+                            BoatIdentity[0] = Identity.DEVIL;
+                        }
+                        else
+                        {
+                            boatchair[1] = temp;
+                            temp.transform.localPosition = BoatSecondClass;
+                            BoatIdentity[1] = Identity.DEVIL;
+                        }
+                    }                   
+                }
+            }
+    }
+
+    public void PriestGeton()
+    {
+        if (this.state == State.LEFT || this.state == State.RIGHT)
+            if (getBoatPassenger() != 0)
+            {
+                if (this.state == State.LEFT)
+                {
+                    if(priests_left.Count != 0)
+                    {
+                        GameObject temp = priests_left.Pop();
+                        temp.transform.parent = Boat.transform;
+                        if (boatchair[0] == null)
+                        {
+                            boatchair[0] = temp;
+                            temp.transform.localPosition = BoatFirstClass;
+                            BoatIdentity[0] = Identity.PRIEST;
+                        }
+                        else
+                        {
+                            boatchair[1] = temp;
+                            temp.transform.localPosition = BoatSecondClass;
+                            BoatIdentity[1] = Identity.PRIEST;
+                        }
+                    }
+                }
+                else
+                {
+                    if(priests_right.Count != 0)
+                    {
+                        GameObject temp = priests_right.Pop();
+                        temp.transform.parent = Boat.transform;
+                        if (boatchair[0] == null)
+                        {
+                            boatchair[0] = temp;
+                            temp.transform.localPosition = BoatFirstClass;
+                            BoatIdentity[0] = Identity.PRIEST;
+                        }
+                        else
+                        {
+                            boatchair[1] = temp;
+                            temp.transform.localPosition = BoatSecondClass;
+                            BoatIdentity[1] = Identity.PRIEST;
+                        }
+                    }
+                }
+            }
+    }
+
+    public void LeftGetoff()
+    {
+        if(this.state == State.LEFT || this.state == State.RIGHT)
+        {
+            if (BoatIdentity[0] != Identity.NONE)
+            {
+                if (BoatIdentity[0] == Identity.DEVIL)
+                {
+                    if (this.state == State.LEFT)
+                        devils_left.Push(boatchair[0]);
+                    else
+                        devils_right.Push(boatchair[0]);
+                }
+                else
+                {
+                    if(this.state == State.LEFT)
+                        priests_left.Push(boatchair[0]);
+                    else
+                        priests_right.Push(boatchair[0]);
+                }
+                boatchair[0].transform.parent = null;
+                boatchair[0] = null;
+                BoatIdentity[0] = Identity.NONE;
+            }
+        }
+    }
+
+    public void RightGetoff()
+    {
+        if (this.state == State.LEFT || this.state == State.RIGHT)
+        {
+            if (BoatIdentity[1] != Identity.NONE)
+            {
+                if (BoatIdentity[1] == Identity.DEVIL)
+                {
+                    if (this.state == State.LEFT)
+                        devils_left.Push(boatchair[1]);
+                    else
+                        devils_right.Push(boatchair[1]);
+                }
+                else
+                {
+                    if (this.state == State.LEFT)
+                        priests_left.Push(boatchair[1]);
+                    else
+                        priests_right.Push(boatchair[1]);
+                }
+                boatchair[1].transform.parent = null;
+                boatchair[1] = null;
+                BoatIdentity[1] = Identity.NONE;
+            }
+        }
+    }
+
+    public void Restart()
+    {
+        EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().name);
+        state = State.LEFT;
+    }
+
+    public void LoadResources()
+    {
+        GameObject MainPage = Instantiate(Resources.Load<GameObject>("prefabs/MainPage"), new Vector3(0, -1, 0), Quaternion.identity);
+        Boat = Instantiate(Resources.Load<GameObject>("prefabs/Boat"), BoatLeftPos, Quaternion.identity);
+        for (int i = 0; i < 3; ++i)
+        {
+            priests_left.Push(Instantiate(Resources.Load("prefabs/Priest")) as GameObject);
+            devils_left.Push(Instantiate(Resources.Load("prefabs/Devil")) as GameObject);
+        }
+    }
+
+    public void judge()
+    {
+        int P_Left = priests_left.Count, P_Right = priests_right.Count, D_Left = devils_left.Count, D_Right = devils_right.Count;
+        int P_Boat = 0, D_Boat = 0;
+        if(D_Right == 3 && P_Right == 3)
+        {
+            this.state = State.WIN;
+            return;
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            if (BoatIdentity[i] == Identity.PRIEST)
+                P_Boat++;
+            else if (BoatIdentity[i] == Identity.DEVIL)
+                D_Boat++;
+        }
+        if(this.state == State.LEFT)
+        {
+            P_Left += P_Boat;
+            D_Left += D_Boat;
+            if(D_Left > P_Left && P_Left != 0)
+            {
+                this.state = State.LOSE;
+            }
+            if (D_Right > P_Right && P_Right != 0)
+            {
+                this.state = State.LOSE;
+            }
+        }
+        else if(this.state == State.RIGHT)
+        {
+            P_Right += P_Boat;
+            D_Right += D_Boat;
+            if (D_Left > P_Left && P_Left != 0)
+            {
+                this.state = State.LOSE;
+            }
+            if (D_Right > P_Right && P_Right != 0)
+            {
+                this.state = State.LOSE;
+            }
+        }
+    }
+
+    public bool isWin()
+    {
+        if (this.state == State.WIN)
+            return true;
+        return false;
+    }
+
+    public bool isLose()
+    {
+        if (this.state == State.LOSE)
+            return true;
+        return false;
+    }
+}
